3c4d1a7471f519d5969074d98f2e031d48a7550d
Me <park.pavel04@gmail.com>
2023-02-17 13:48:28 +0600

    Add and Read Functions

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ class Car:
         self.type = type;
         self.power = power;
 
+class CarShowRoom:
+    def __init__(self):
+        self.cars = []
+
+    def add(self, name, price, type, power):
+        car = Car(name, price, type, power)
+        self.cars.append(car)
+        print(f"{name} added")
+
+    def read(self):
+        if not self.cars:
+            print("No Cars")
+        for i, car in enumerate(self.cars):
+            print(f"{i + 1}. {car.name} {car.type} ({car.power}) - ${car.price}")
