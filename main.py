f353c5d367f19cca56b2c71000aa71815791ead3
Me <park.pavel04@gmail.com>
2023-02-17 13:21:18 +0600

    Add Object

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ class Car:
     name = None;
     price = None;
     type = None;
+    Power = None;
+
+
+#Add Object
+car1 = Car()
+car1.name = "McLaren"
+car1.price = "320.000$"
+car1.type = "Sport Car"
+car1.Power = "600HP"
+
